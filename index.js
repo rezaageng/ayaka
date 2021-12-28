90c69aa220a8c40c1333b6da0a44b2eb5fa55f0a
Reza Ageng <rezaageng5@gmail.com>
2021-12-28 21:34:59 +0700

    add some log

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const Discord = require("discord.js")
-const {TOKEN} = require('./config.json')
+const {TOKEN} = require("./config.json")
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 })
 
 client.on("ready", () => {
     console.log(`${client.user.tag} is here :D`)
+})
+client.on("reconnecting", () => {
+    console.log(`${client.user.tag} is reconnecting`)
+})
+client.on("disconnect", () =>{
+    console.log(`${client.user.tag} has left you`)
 })
 
 client.on("messageCreate", (msg) => {
