638646cba74bc7585da3df2f5d2e40dc7b55ffde
Reza Ageng <rezaageng5@gmail.com>
2022-01-01 10:45:52 +0700

    welcome messeges

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 const client = new Discord.Client({
-    intents: ["GUILDS", "GUILD_MESSAGES"]
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILDS_MEMBERS"]
 })
 
 client.on("ready", () => {
@@ -12,6 +12,13 @@ client.on("messageCreate", (msg) => {
     if (msg.content == "hi") {
         msg.reply("halo sayang <3")
     }
+})
+
+// welcome messeges
+const welcomeChId = "883715121019486240"
+
+client.on("guildMemberAdd", (member) => {
+    member.guild.channels.cache.get(welcomeChId).send(`<@${member.id}> welcome to the server :D`)
 })
 
 client.login(process.env.TOKEN)
