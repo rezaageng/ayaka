5b2dc02e7dba701e0b0b85ae47ed4b1c22f17cb1
Reza Ageng <rezaageng5@gmail.com>
2022-01-15 10:21:57 +0700

    index

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 const Discord = require("discord.js")
 require("dotenv").config()
-const generateImage = require("./generateImage")
+// const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 })
 
-client.on("ready", () => {
-    console.log(`${client.user.tag} is here :D`)
-    client.user.setActivity("You ｏ(≧▼≦○〃", { type: "WATCHING" })
-    require("http")
-        .createServer((req, res) => res.end(`${client.user.tag} is here :D`))
-        .listen(3000)
-})
+let bot = {
+    client,
+    prefix: "ay.",
+    owners: "465403883469012992",
+}
 
-client.on("messageCreate", (msg) => {
-    if (msg.content == "hi") {
-        msg.reply("hii <3")
-    }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-// welcome messeges
-const welcomeChId = "883715121019486240"
+// client.on("ready", () => {
+//     console.log(`${client.user.tag} is here :D`)
+//     client.user.setActivity("You ｏ(≧▼≦○〃", { type: "WATCHING" })
+//     require("http")
+//         .createServer((req, res) => res.end(`${client.user.tag} is here :D`))
+//         .listen(3000)
+// })
 
-client.on("guildMemberAdd", async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChId).send({
-        content: `<@${member.id}> welcome to the server :D`,
-        files: [img],
-    })
-})
+// client.on("messageCreate", (msg) => {
+//     if (msg.content == "hi") {
+//         msg.reply("hii <3")
+//     }
+// })
 
-client.login(process.env.TOKEN)
+// // welcome messeges
+// const welcomeChId = "883715121019486240"
+
+// client.on("guildMemberAdd", async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChId).send({
+//         content: `<@${member.id}> welcome to the server :D`,
+//         files: [img],
+//     })
+// })
+
+// client.login(process.env.TOKEN)
