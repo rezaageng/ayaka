1792c1f44730fcec6195987a7fc6035261741943
Reza Ageng <rezaageng5@gmail.com>
2022-01-15 20:39:32 +0700

    welcome msg

---
+++ events/welcomeMessage.js
@@ -0,0 +1,90 @@
+const Canvas = require("canvas")
+const Discord = require("discord.js")
+const welcomeChId = "925298083556888586"
+
+const background = "https://i.imgur.com/lHOV1fj.png"
+
+const dim = {
+    width: 1200,
+    height: 675,
+}
+
+const Avatar = {
+    size: 256,
+    x: 480,
+    y: 170,
+}
+
+Canvas.registerFont("./assets/fonts/coolvetica rg.otf", {
+    family: "Coolvetica Rg",
+})
+Canvas.registerFont("./assets/fonts/MochiyPopOne-Regular.ttf", {
+    family: "Mochiy Pop One",
+})
+
+module.exports = {
+    name: "welcomeMessage",
+    run: async (bot, member) => {
+        const regex =
+            /[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]/
+        let username = member.user.username
+        let discrim = member.user.discriminator
+        let avatarURL = member.user.displayAvatarURL({
+            format: "png",
+            dynamic: false,
+            size: Avatar.size,
+        })
+
+        const canvas = Canvas.createCanvas(dim.width, dim.height)
+        const ctx = canvas.getContext("2d")
+
+        const bgImg = await Canvas.loadImage(background)
+        ctx.drawImage(bgImg, 0, 0)
+
+        ctx.fillStyle = "rgba(0, 0, 0, 0.5)"
+        ctx.fillRect(0, 0, dim.width, dim.height)
+
+        const avImg = await Canvas.loadImage(avatarURL)
+        ctx.save()
+
+        ctx.beginPath()
+        ctx.arc(
+            Avatar.x + Avatar.size / 2,
+            Avatar.y + Avatar.size / 2,
+            Avatar.size / 2,
+            0,
+            Math.PI * 2,
+            true
+        )
+        ctx.closePath()
+        ctx.clip()
+
+        ctx.drawImage(avImg, Avatar.x, Avatar.y)
+        ctx.restore()
+
+        ctx.fillStyle = "white"
+        ctx.textAlign = "center"
+
+        ctx.font = "70px 'Coolvetica Rg'"
+        ctx.fillText("WELCOME", dim.width / 2, 130)
+
+        username.match(regex)
+            ? (ctx.font = "50px 'Mochiy Pop One'")
+            : (ctx.font = "60px 'Coolvetica Rg'")
+        ctx.fillText(`${username}#${discrim}`, dim.width / 2, 500)
+
+        ctx.font = "40px 'Coolvetica Rg'"
+        ctx.fillText("Have a nice day :D", dim.width / 2, 550)
+
+        const attachement = new Discord.MessageAttachment(
+            canvas.toBuffer(),
+            "welcome.png"
+        )
+
+        // const img = await generateImage(member)
+        member.guild.channels.cache.get(welcomeChId).send({
+            content: `<@${member.id}> welcome to the server :D`,
+            files: [attachement],
+        })
+    },
+}
--- handlers/events.js
+++ handlers/events.js
@@ -50,4 +50,8 @@ function initEvents(bot) {
     client.on("messageCreate", (message) => {
         triggerEventHandler(bot, "messageCreate", message)
     })
+
+    client.on("guildMemberAdd", (member) => {
+        triggerEventHandler(bot, "welcomeMessage", member)
+    })
 }
