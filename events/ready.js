824835985cdfea4879544e56b33d184934fe002b
Reza Ageng <rezaageng5@gmail.com>
2022-01-15 12:06:58 +0700

    set activity

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,6 @@ module.exports = {
     name: "ready",
     run: async (bot) => {
         console.log(`${bot.client.user.tag} is here :D`)
+        bot.client.user.setActivity("You ｏ(≧▼≦○〃", { type: "WATCHING" })
     },
 }
