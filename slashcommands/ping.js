3f1e902897f04ad7830f446360a10fff81736e5a
Reza Ageng <rezaageng5@gmail.com>
2022-01-29 21:34:10 +0700

    ping command

---
+++ slashcommands/ping.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+  name: "ping",
+  description: "Ping ayaka",
+  permissions: [],
+  devOnly: false,
+  run: async ({ client, interaction, args }) => {
+    const exampleEmbed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Pong")
+
+      .addField("Latency", `🏓 ${Date.now() - interaction.createdTimestamp}ms`)
+      .addField("API Latency", `🔥 ${Math.round(client.ws.ping)}ms`)
+      .setTimestamp()
+
+    return interaction.reply({ embeds: [exampleEmbed] })
+  },
+}
