1da9c16299743fca4c0ec3ea61758dd46da1f31d
Reza Ageng <rezaageng5@gmail.com>
2022-02-02 14:08:03 +0700

    fix multiple destroyed queue

--- events/connectionError.js
+++ events/connectionError.js
@@ -1,6 +1,7 @@
 module.exports = {
   name: "connectionError",
   run: async (bot, queue, error) => {
-    queue.metadata.channel.send("An error occurred while playing.")
+    queue.metadata.channel.send("Connection Error")
+    console.error(error)
   },
 }
