521ead88bce7fe142a2bc54910a821c81d9fc6f0
Reza Ageng <rezaageng5@gmail.com>
2022-02-06 14:25:31 +0700

    update rng

--- util/apiRequest.js
+++ util/apiRequest.js
@@ -5,7 +5,7 @@ async function getGiphy(query) {
   const rng = Math.floor(Math.random() * 40)
   try {
     const response = await axios.get(
-      `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY}&q=${query}&limit=40`
+      `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY}&q=${query}&limit=50`
     )
 
     return response.data.data[rng].images.original.url
