ba20b2e96622b02cd0b0a1e0c0a6cfef41c75a6b
Reza Ageng <rezaageng5@gmail.com>
2022-02-14 13:00:59 +0700

    add first rabbit

---
+++ slashcommands/client/firstRabbit.js
@@ -0,0 +1,35 @@
+module.exports = {
+  name: "first-rabbit",
+  description: "you cant use this command",
+  category: "client",
+  permissions: [],
+  devOnly: true,
+  options: [
+    {
+      name: "member",
+      description: "??",
+      type: "USER",
+      required: true,
+    },
+    {
+      name: "message",
+      description: "?????",
+      type: "STRING",
+      required: true,
+    },
+  ],
+  run: async ({ client, owners, interaction }) => {
+    const member = interaction.options.getMember("member")
+    const message = interaction.options.getString("message")
+
+    if (owners !== interaction.member.id)
+      return await interaction.reply({
+        content: `Sorry, only <@${owners}> can use this command!`,
+        ephemeral: true,
+      })
+
+    member.send(message)
+
+    return await interaction.reply(`To: ${member} msg: ${message}`)
+  },
+}
